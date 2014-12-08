656a50f984c79b9d23f4ec26969aeaf287f4ab38
Ryuji Miyai <ryuji.bump@gmail.com>
2014-12-09 00:32:54 +0900

    入室したユーザーのカウント機能追加

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ http.listen(8080,function(){
 var userHash = {};
 var line = [];
 //接続数のカウンタ
-var counter = 0;
+var counter = 0,
+//入室したユーザーのカウント
+    joinUser = 0;
 
 //ルーティング
 app.use(express.static(__dirname + '/'));
@@ -29,10 +31,12 @@ io.sockets.on("connection", function (socket) {
 
     // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
     socket.on("connected", function (name) {
-    var msg = name + "が入室しました";
-    userHash[socket.id] = name;
-    io.sockets.emit("publish", {value: msg});
-    line.push(msg);
+        joinUser++;
+        console.log('user:' + joinUser);
+        var msg = name + "が入室しました";
+        userHash[socket.id] = name;
+        io.sockets.emit("publish", {value: msg});
+        line.push(msg);
     });
 
     // メッセージ送信カスタムイベント    
@@ -46,9 +50,11 @@ io.sockets.on("connection", function (socket) {
     // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
     socket.on("disconnect", function () {
     if (userHash[socket.id]) {
-      var msg = userHash[socket.id] + "が退出しました";
-      delete userHash[socket.id];
-      io.sockets.emit("publish", {value: msg});
+        joinUser--;
+        console.log('user:' + joinUser);
+        var msg = userHash[socket.id] + "が退出しました";
+        delete userHash[socket.id];
+        io.sockets.emit("publish", {value: msg});
     }
     counter--;
     console.log("connect:" + counter);
